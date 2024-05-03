844d7bde35466303b7d53528cc2a8d5881fca82b
DaleMcBride21 <dlmcburrito@gmail.com>
2024-05-02 20:51:19 -0600

    udpate

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,26 @@
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // Create Job instances
+        Job job1 = new Job("Software Engineer", "Microsoft", 2019, 2022);
+        Job job2 = new Job("Manager", "Apple", 2022, 2023);
+
+        // Display job details
+        job1.Display();
+        job2.Display();
+
+        // Create Resume instance
+        Resume myResume = new Resume("Dale McBride");
+
+        // Add jobs to the resume
+        myResume.AddJob(job1);
+        myResume.AddJob(job2);
+
+        // Display resume
+        myResume.Display();
     }
 }
